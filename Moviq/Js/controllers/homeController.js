420751e6a2bc46df4a52aa16a10d2a4d62849fdc
AviniAgarwal <avinia@andrew.cmu.edu>
2014-12-02 20:46:32 -0500

    stripe changes
    needs modification

--- Moviq/Js/controllers/homeController.js
+++ Moviq/Js/controllers/homeController.js
@@ -4,7 +4,7 @@ define('controllers/homeController', {
     init: function (routes, viewEngine, Products, Product, Cart, CartItem) {
         "use strict";
 
-        var onSearch, onCart;
+        var onSearch, onCart, onPayment, onStripeSubmit;
 
         // GET /#/search/?q=searchterm
         // search for products
@@ -68,10 +68,54 @@ define('controllers/homeController', {
             });
         };
 
-        return {
-            onSearch: onSearch,
-            onCart: onCart
+        routes.get('/payment', function () {
+            routes.navigate('/payment/?q=' + $('#cart-Total').val());
+        });
+
+        routes.get(/^\/payment\/?/i, function (context) {
+            onPayment(context);
+        });
+
+        onPayment = function (context) {
+            viewEngine.setView({
+                template: 't-payment-info'
+            });
+        }
+        
+        onStripeSubmit = function (event) {
+            var $form = $(this);
+
+            // Disable the submit button to prevent repeated clicks
+            $form.find('button').prop('disabled', true);
+
+            Stripe.card.createToken($form, stripeResponseHandler);
+
+            // Prevent the form from submitting with the default action
+            return false;
         };
 
+        function stripeResponseHandler(status, response) {
+            var $form = $('#payment-form');
+
+            if (response.error) {
+                // Show the errors on the form
+                $form.find('.payment-errors').text(response.error.message);
+                $form.find('button').prop('disabled', false);
+            } else {
+                // response contains id and card, which contains additional card details
+                var token = response.id;
+                // Insert the token into the form so it gets submitted to the server
+                $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+                // and submit
+                $form.get(0).submit();
+            }
+        };
+
+        return {
+            onSearch: onSearch,
+            onCart: onCart,
+            onPayment: onPayment,
+            onStripeSubmit: onStripeSubmit
+        };
     }
 });
